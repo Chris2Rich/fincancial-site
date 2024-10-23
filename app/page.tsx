db2dc3f89feec3317ca13585810b9ecea03079a3
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-10-23 07:42:19 +0000

    smaller text

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-"use client"
 import Image from "next/image";
 import React from "react"
 import { BackgroundBeams } from "../components/ui/background-beams"
 import { FlipWords } from "../components/ui/flip-words"
 
 export function HeroText() {
-  const words = ["insights", "growth", "inflation"]
+  const words = ["new key insights", "sources of alpha", "your next move"]
  
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-        Visualize <FlipWords words={words}/>
+      <div className="relative z-10 text-lg md:text-3xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+        Discover <FlipWords words={words} duration={5000}/>
       </div>
     </div>
   )
@@ -18,7 +17,7 @@ export function HeroText() {
 
 export function Background() {
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="h-[30rem] rounded-md w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <HeroText></HeroText>
       </div>
