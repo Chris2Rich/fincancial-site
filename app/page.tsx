727a24a53be93b5ec892988456a86a3e71e7f495
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-10-24 08:55:01 +0000

    !!!!!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Image from "next/image"
 import React from "react"
 import { BackgroundBeams } from "@/components/ui/background-beams"
 import { FlipWords } from "@/components/ui/flip-words"
 import { TextHoverEffect } from "@/components/ui/text-hover-effect"
+import { HeroParallax } from "@/components/ui/hero-parallax"
 
 function HeroText() {
   const words = ["new, key insights", "sources of alpha", "your next trade"]
@@ -20,25 +21,35 @@ function HeroText() {
   )
 }
 
+const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "/tt.png",
+  }
+]
+
 export default function Home() {
   return (
     <div className="h-screen w-screen overflow-hidden">
       <div className="h-full w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
-          <HeroText />
+          <HeroText/>
         </div>
-        <a href="/">
+        <a href="/#product">
           <div className="relative mt-10 z-10">
             <button className="relative group hover:scale-125 hover:duration-300 duration-300">
               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-200"/>
               <div className="relative px-8 py-2 rounded-lg text-white bg-gradient-to-r from-indigo-700 to-purple-700">
-                Explore
+                Demo
               </div>
             </button>
           </div>
         </a>
         <BackgroundBeams />
       </div>
+      <HeroParallax products={products}></HeroParallax>
     </div>
   )
 }
